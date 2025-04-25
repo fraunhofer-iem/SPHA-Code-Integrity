2745fac21943b4ab536759e0c127bc1d1c9d1011
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2025-04-25 18:29:36 +0200

    added multi repo support

--- cmd/multiRepo/Main.go
+++ cmd/multiRepo/Main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"os"
 	"path"
+	"project-integrity-calculator/internal/io"
 	"project-integrity-calculator/internal/logging"
+	"project-integrity-calculator/internal/processor"
 	"strings"
 	"time"
 )
 
 var (
-	ownerAndRepo = flag.String("ownerAndRepo", "", "GitHub repository link (e.g., https://github.com/owner/repo)")
-	token        = flag.String("token", "", "GitHub access token")
-	targetBranch = flag.String("branch", "", "Target branch to analyze. Defaults to the default branch of the repository")
-	mode         = flag.String("mode", "local", "Mode: 'local' or 'clone'")
-	localPath    = flag.String("localPath", "", "Path to the local repository (required if mode is 'local')")
-	cloneTarget  = flag.String("cloneTarget", "", "Target to clone. Defaults to tmp")
-	logLevel     = flag.Int("logLevel", 0, "Can be 0 for INFO, -4 for DEBUG, 4 for WARN, or 8 for ERROR. Defaults to INFO.")
+	token       = flag.String("token", "", "GitHub access token")
+	mode        = flag.String("mode", "local", "Mode: 'local' or 'clone'")
+	cloneTarget = flag.String("cloneTarget", "", "Target to clone. Defaults to tmp")
+	logLevel    = flag.Int("logLevel", 0, "Can be 0 for INFO, -4 for DEBUG, 4 for WARN, or 8 for ERROR. Defaults to INFO.")
+	out         = flag.String("out", "", "Directory to which the output is written. Defaults to the current working directory.")
+	in          = flag.String("in", "", "Input file with the repositories to process.")
 )
 
 func main() {
@@ -27,25 +29,64 @@ func main() {
 
 	logger := logging.SetUpLogging(*logLevel)
 
-	if *ownerAndRepo == "" {
-		panic("ownerAndRepo is required")
+	if *in == "" {
+		panic("in is required")
 	}
-
-	ownerAndRepoSplit := strings.Split(*ownerAndRepo, "/")
-	// TODO: do validation for format
 
 	if *token == "" {
 		panic("token is required")
 	}
 
-	if *mode == "local" && *localPath == "" {
-		panic("localPath is required if mode is 'local'")
+	if *cloneTarget == "" {
+		*cloneTarget = path.Join(os.TempDir(), "codeintegrity")
 	}
 
-	if *mode == "clone" && *cloneTarget == "" {
-		*cloneTarget = path.Join(os.TempDir(), "codeintegrity", ownerAndRepoSplit[1])
+	if *out == "" {
+		wd, err := os.Getwd()
+		if err != nil {
+			panic("Couldn't get workind directory")
+		}
+		*out = wd
 	}
 
+	file, err := os.Open(*in)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	var input io.Input
+	if err := decoder.Decode(&input); err != nil {
+		panic(err)
+	}
+	for _, r := range input.Data.Search.Nodes {
+
+		ownerAndRepoSplit := strings.Split(r.NameWithOwner, "/")
+
+		clonePath := path.Join(*cloneTarget, ownerAndRepoSplit[1])
+		config := processor.RepoConfig{
+			Owner:     ownerAndRepoSplit[0],
+			Repo:      ownerAndRepoSplit[1],
+			LocalPath: "",
+			ClonePath: clonePath,
+			Branch:    "",
+			Token:     *token,
+			Out:       *out,
+		}
+
+		repo, err := processor.ProcessRepo(config)
+		if err != nil {
+			panic(err)
+		}
+
+		*out = path.Join(*out, config.Owner+config.Repo+"-result.json")
+		err = io.StoreResult(*out, *repo)
+		if err != nil {
+			panic(err)
+		}
+	}
 	elapsed := time.Since(start)
 	logger.Info("Execution finished", "time elapsed", elapsed)
 }
